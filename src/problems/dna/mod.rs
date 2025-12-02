59fd02749fbdeb2bd28cd74ea5104974ab4912b9
van-sprundel <ramonvansprundel@gmail.com>
2025-12-03 00:28:26 +0100

    use mapping

--- src/problems/dna/mod.rs
+++ src/problems/dna/mod.rs
@@ -1,13 +1,19 @@
 pub fn solve(input: &str) -> String {
-    let [a_count, c_count, g_count, t_count] = input.chars().fold([0usize; 4], |mut acc, c| {
-        acc[match c {
-            'A' => 0,
-            'C' => 1,
-            'G' => 2,
-            'T' => 3,
-            _ => return acc,
-        }] += 1;
-        acc
-    });
+    static LUT: [u8; 256] = {
+        let mut t = [4u8; 256];
+        t[b'A' as usize] = 0;
+        t[b'C' as usize] = 1;
+        t[b'G' as usize] = 2;
+        t[b'T' as usize] = 3;
+        t
+    };
+
+    let mut acc = [0usize; 4];
+    for &b in input.as_bytes() {
+        let idx = LUT[b as usize] as usize;
+        acc[idx & ((idx < 4) as usize * idx)] += ((idx < 4) as usize);
+    }
+
+    let [a_count, c_count, g_count, t_count] = acc;
     format!("{a_count} {c_count} {g_count} {t_count}")
 }
